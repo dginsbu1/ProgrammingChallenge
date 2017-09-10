4daf24ea5a95538afc2ea20d7f722e48de56c0f0
Daniel Ginsburg <dginsbu1@mail.yu.edu>
2017-09-09 22:35:53 -0400

    Hello World

---
+++ YUGIT/IntroToCompSci/homework/very_second_program/src/main/java/AppT.java
@@ -0,0 +1,8 @@
+/** * Hello world! * */ 
+public class AppT {  
+	public static void main( String[] args ) {
+   		 System.out.println("Daniel Ginsburg");    
+		 System.out.println("dginsbu1");  
+  		 System.out.println("dginsbu1@mail.yu.edu");  
+  		   							} 
+		}
