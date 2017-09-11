7b5662e26775a1c1c7335c6aa8a29304189e6d9b
Daniel Ginsburg <dginsbu1@mail.yu.edu>
2017-09-11 19:03:21 -0400

    fix dir structure

---
+++ IntroToCompSci/homework/variables1/src/main/java/App.java
@@ -0,0 +1,12 @@
+/** * Hello world! * */ 
+public class App {  
+	public static void main( String[] args ) {
+		int pillar = 3;
+		double notPillar = 2.52;
+		boolean truePillar = true;
+   		 System.out.println("Hello World!");    
+		 System.out.println("The world stands on " + pillar + " pillars");  
+  		 System.out.println("Not " + notPillar + " pillars");  
+  		 System.out.println("and ... this is a very " + truePillar + " statement");
+  							} 
+		}
