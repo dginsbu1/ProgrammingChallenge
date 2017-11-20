accb79dffd1d9cbb6010fad45809b4821e7bb11d
Daniel Ginsburg <dginsbu1@mail.yu.edu>
2017-11-20 18:16:56 -0500

    11/20

--- IntroToCompSci/homework/objects/RationalNumbers/src/main/java/edu/yu/intro/objects/rational/Rational.java
+++ IntroToCompSci/homework/objects/RationalNumbers/src/main/java/edu/yu/intro/objects/rational/Rational.java
@@ -1,15 +1,19 @@
 package edu.yu.intro.objects.rational;
+import java.io.*;
 
 public class Rational{
 	int numerator, denominator;
 
+	public Rational(int num, int den){
+		numerator = num;
+		denominator = den;
+	}
 	public Rational(){
-		numerator = 0;
-		denominator = 1;
+		this(0,1);
 	}
 	//Write an instance method called printRational() that displays a Rational in some reasonable format
 	public void printRational() {
-		System.out.printf("%d/%d", numerator,denominator);
+		System.out.printf("%d/%d %n", numerator,denominator);
 	}
 	public int getNumerator(){
 		return numerator;
@@ -18,28 +22,37 @@ public class Rational{
 		return denominator;
 	} 
 	public static void main(String[] args) {
-		Rational rat = new Rational();
-		System.out.print("current rational: ");
-		rat.printRational();
-		Rational rat2 = new Rational(2,3);
+		Rational rat1 = new Rational();
+		rat1.printRational();
 		
-		rat2.negate();
-		System.out.print("\ncurrent rational: ");
+		Rational rat2 = new Rational(3,2);
 		rat2.printRational();
 
-		rat2.invert();
-		System.out.print("\ncurrent rational: ");
+		rat2.negate();//make negative
 		rat2.printRational();
 
-		System.out.printf("%nThe double version is: %f", rat2.toDouble());
+		rat2.invert();//flip num and denom
+		rat2.printRational();
+		System.out.printf("The double version of %d/%d is: %f %n", rat2.getNumerator(), rat2.getDenominator(), rat2.toDouble());
+
+		Rational rat3 = new Rational(20,24);
+		rat3.printRational();
+
+		rat3 = rat3.reduce();
+		rat3.printRational();
+
+		rat3 = rat3.add(new Rational(3,4));
+		rat3.printRational();
+
+		rat3 = rat3.add(new Rational (0,23));
+		rat3.printRational();		
+		
 	}
-	public Rational(int num, int den){
-		numerator = num;
-		denominator = den;
-	}
+	//makes the fracction negative
 	public void negate(){
 		numerator *= -1;
 	}
+	//flips the numerator and denominator
 	public void invert(){
 		int num = numerator;
 		numerator = denominator;
@@ -49,22 +62,33 @@ public class Rational{
 		return ((double)numerator / denominator);
 	}
 	public Rational reduce(){
+		if(numerator==0){
+			return new Rational(0,denominator);
+		}
 		int num = numerator;
 		int den = denominator;
 		while(den>0){
+			//using Euclideans algorithm 
 			//i is a holding variable for the numerator
 			int i = num;
 			num = den;
 			den = i%den;
 		}
-		return new Rational(num, den);
+		//num is the gcd
+		return new Rational(numerator/num, denominator/num);
 	}
+	//adds two fractions 
 	public Rational add(Rational rat){
+		if(rat.getDenominator()==0){
+			throw new IllegalArgumentException("You tried to add a number with 0 as the denominator");
+		}
+		if(this.getDenominator()==0){
+			throw new IllegalStateException("You tried to add a number with 0 as the denominator");
+		}
 		int newDen = this.denominator*rat.getDenominator();
-		int newNum = this.numerator*rat.getDenominator()+rat.getNumerator()*this.numerator;
+		int newNum = this.numerator*rat.getDenominator()+rat.getNumerator()*this.denominator;
 		Rational newRat = new Rational(newNum,newDen);
 		return newRat.reduce();
-
 	}
 
 
