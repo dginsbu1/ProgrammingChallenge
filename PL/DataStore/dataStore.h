02ce9643468b4b1153e538b27a36a476c61b93ae
Daniel Ginsburg <dginsbu1@mail.yu.edu>
2018-10-31 16:46:13 -0400

    "test"

---
+++ PL/DataStore/dataStore.h
@@ -0,0 +1,18 @@
+dataStore.h
+
+
+int addCar(char* make, char* model, short year, long price, int uniqueID);
+int setMaxMemory(size_t bytes);
+struct Car* getCarById(int id);
+int deleteCarById(int id);
+int modifyCarById(int id, struct Car* myCar);
+int getNumberOfCarsInMemory();
+int getAmountOfUsedMemory();
+int getNumberOfCarsOnDisk();
+struct Car* getAllCarsInMemory();
+/* this function does NOT cause the cars on disk to displace those
+ * that were already in memory. It uses separate memory to load them
+ * and return them to the caller. THE CALLER MUST FREE THIS MEMORY
+ * WHEN FINISHED WITH THESE CARS.
+*/
+struct Car* getAllCarsOnDisk();
