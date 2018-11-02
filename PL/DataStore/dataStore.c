f40cb4f7608aa65db5cb462e0c9a2aa41e748ed7
Daniel Ginsburg <dginsbu1@mail.yu.edu>
2018-11-01 20:23:37 -0400

    "test2"

--- PL/DataStore/dataStore.c
+++ PL/DataStore/dataStore.c
@@ -36,43 +36,14 @@ int setMaxMemory(size_t bytes){
     mallocCalled = 1;
     return 0;
 }
+//DONE
 //make a new Car with the given parameters
 //Test: idExists, noMemory, yesMemory,
 int addCar(char* make, char* model, short year, long price, int uniqueID){
-    //DON'T NEED TO CHECK
-    //dont allow 0 as id
-//    if(uniqueID == 0){
-//        //throw some error
-//    }
-//    //TODO
-//    if(make > 16 | model > 16){/*throw error*/}
-//    if(!isUniqueID()) {
-//        //throw some error or something
-//    }
     Car newCar = Car{make, model, year, price, uniqueID};
-    //check memory
-    if(currentCarsAvailable){//not zero (there is space)
-        //check to make sure the * is in right place
-        *(myCars+getNumberOfCarsInMemory()) = newCar;
-        currentCarsAvailable--;
-    }
-    //there is NO space
-    else{
-        //TODO
-        //write to file
-    }
-    carTotal++;
+    addCar(newCar);
     return 0;
 }
-
-
-
-//DON'T NEED
-//returns 0/false if a car with id was found
-//returns 1/true if no car was found with id
-//static int isUniqueID(int id){
-//    return findCar == NULL;
-//}
 
 //retrieve car with given id
 //if no car found return 0
@@ -80,22 +51,59 @@ int addCar(char* make, char* model, short year, long price, int uniqueID){
 //if(in memory) take out and shift; else if full then write last car to disk
 struct Car* getCarById(int id){
     Car* car
-    int position = getPositionInMemory(id);
-    car = shiftCarsRight;
-    if(car == NULL) {
-        car = findCarOnDisk();
-    }if(car == NULL){
-        return NULL;
+    int position = getPositionInMemory(id);//lowest position is 1
+    if(position) {//it was found
+        car = shiftCarsRight(positon);
+        currentCarsAvailable++;//just "removed a car" will add back soon
     }
+    else{
+        car = getCarFromDisk(id);
+    }
+    //put car in new slot
+    addCar(car);
     return &car;
-
 }
+
+//adds Car to available slot memory or disk
+//TODO
+int addCar(car){
+    //check memory
+    //if memory not initialized yet
+    if(initialCarsAvailable == 0){
+        writeToDisk(car);
+    }
+    else if(currentCarsAvailable){//not zero (there is space)
+        //check to make sure the * is in right place
+        //either need to shift and add it
+        //or its been shifted and need to add it
+        myCars[0] = car;
+        currentCarsAvailable--;
+    }
+        //there is NO space so we MAKE IT
+    else{//write last car to file, shift all cars over, put in place
+        //write last car to file, shift all right, put in first spot
+        writeToDisk(myCars[rightShift(initialCarsAvailable)]);//TODO off by one?
+    }
+    carTotal++;
+    return 0;
+}
+
+
+//DON'T NEED
+//returns 0/false if a car with id was found
+//returns 1/true if no car was found with id
+static int isUniqueID(int id){
+    return findCar == NULL;
+}
+
+
 //DONE
 //shifts all the cars from the given position to the right
 //return the car at the given position
 //makes room for new car
 struct Car* shiftCarsRight(int position){
     //save the car at position before it get overridden
+    position--;//positions are one off from "true" position
     Car removedCar*;
     removedCar = (myCars+position)*;
     for(int i = position; i > 0; i--){
@@ -103,20 +111,34 @@ struct Car* shiftCarsRight(int position){
     }
     return removedCar;
 }
+//DONE
+//returns the position of car in memory or 0 if not found
+int getPositionInMemory(int id){
+    for(int i = 0; i < getNumberOfCarsInMemory(); i++){
+        if(myCars[i].id == id){
+            return i+1;//0 reserved for NULL
+        }
+    }
+    return 0;
+}
 
+//returns a pointer to the car in memory
+//also deletes the car from the disk
+//TODO
+struct Car* getCarFromDisk(id){
+    //scan file,
+    //  check for id (list ID first),
+    //      if(id == id) scan the other items and make new car and delete car and shift all up
+    //      if(id == id) return 0;
 
-
-
-
-
-
+}
 //
 int deleteCarById(int id){
-
     carTotal--;
     //change currentCarsAvailable
     return 0;
 }
+
 int modifyCarById(int id, struct Car* myCar);
 
 //DONE
